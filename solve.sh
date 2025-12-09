477e1fd4ecd7c6544435f6401b1c58256bce77ff
spritezs <tudor.huica@gmail.com>
2025-12-09 13:22:31 +0000

    q

--- solve.sh
+++ solve.sh
@@ -7,26 +7,29 @@ fi
 MODEL_TYPE="$1"
 export MODEL_TYPE
 
-# ONLY RUN THIS FOR ONE MODEL AT A TIME. CANT RUN MANY MODELS IN PARALLEL
-
-find ./Params/*.param | parallel --j 80  --memfree 200G '
-    param_file={};
+for param_file in ./Params/*.param; do
     param_name=$(basename "$param_file" .param)
     echo "Processing parameter file: $param_name"
 
     # Create a unique output directory for each parallel task
-    output_dir="./Output/conjure-output-${MODEL_TYPE}-${param_name}";
+    output_dir="./Output/conjure-output-${MODEL_TYPE}-${param_name}"
     rm -rf "$output_dir"
-    mkdir -p "$output_dir";
+    mkdir -p "$output_dir"
 
     conjure --output-directory="$output_dir" ./Models/${MODEL_TYPE}.essence -ac
 
-    conjure translate-parameter --eprime "$output_dir"/model000001.eprime --essence-param "$param_file" --eprime-param "$output_dir"/eprime.params
+    conjure translate-parameter \
+        --eprime "$output_dir/model000001.eprime" \
+        --essence-param "$param_file" \
+        --eprime-param "$output_dir/eprime.params"
 
     python modify_model.py "$param_name" "$output_dir" "$MODEL_TYPE"
-    cp ./Final-models/${MODEL_TYPE}-final/rel_dom.essence "$output_dir"
+    cp "./Final-models/${MODEL_TYPE}-final/rel_dom.essence" "$output_dir"
 
-    ../conjure-oxide/target/release/conjure-oxide solve --no-use-expand-ac  --output ${output_dir}/sols.json  ${output_dir}/${MODEL_TYPE}.eprime
+    ../conjure-oxide/target/release/conjure-oxide solve \
+        --no-use-expand-ac \
+        --output "${output_dir}/sols.json" \
+        "${output_dir}/${MODEL_TYPE}.eprime"
 
     if [[ "$MODEL_TYPE" == "RC" ]]; then
         python filter_dominant_solutions_RC.py "$output_dir"
@@ -37,10 +40,6 @@ find ./Params/*.param | parallel --j 80  --memfree 200G '
         exit 1
     fi
 
-    # # # --human-rule-trace=trace.txt
-
-    mv "$output_dir"/non-dominated.json "./Solutions/${param_name}_${MODEL_TYPE}_non-dominated.json"
-'
-
-
+    mv "$output_dir/non-dominated.json" "./Solutions/${param_name}_${MODEL_TYPE}_non-dominated.json"
+done
 
